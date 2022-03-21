bd0119c24e2cb1c0d0f665f4f28fdb54b757de3d
frcavalli <101572991+frcavalli@users.noreply.github.com>
2022-03-21 16:20:41 +0100

    Update myfirtsapp.py

--- myfirtsapp.py
+++ myfirtsapp.py
@@ -39,3 +39,33 @@ weatherData = json.loads(response.text)
 # pprint(weatherData) 
 
 st.write(weatherData['main']['temp_max']) 
+
+-----------------------------------------------------------------------------
+
+ # ! python3
+import json, requests 
+
+# add your own APIkey
+APIkey = "914e56ce07698c06f712a3cad0747080"
+location = st.text_input("Give me the name of a city")
+
+# check API documentation to see what structure of URL is needed to access the data
+# http://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
+url = 'http://api.openweathermap.org/data/2.5/weather?q=' + location + '&appid=' + APIkey
+# print(url)
+
+
+# Download the JSON data from OpenWeatherMap.org's API.
+response = requests.get(url)  
+# Uncomment to see the raw JSON text:
+# print(response.text)  
+
+
+# Load JSON data into a Python variable.
+weatherData = json.loads(response.text)
+# Uncomment to see the raw JSON text:
+st.write(weatherData) 
+# from pprint import pprint 
+# pprint(weatherData) 
+
+st.write(weatherData['main']['temp_max']) 
