53f8d99ba079f8bb7651034869c5a37fe0d2d9a7
frcavalli <101572991+frcavalli@users.noreply.github.com>
2022-03-21 16:13:20 +0100

    Update myfirtsapp.py

--- myfirtsapp.py
+++ myfirtsapp.py
@@ -11,4 +11,30 @@ if genre == 'Comedy':
 else:
     st.write("You didn't select comedy.")
 
-  
+  # ! python3
+import json, requests 
+
+# add your own APIkey
+APIkey = "914e56ce07698c06f712a3cad0747080"
+location = input("Give me the name a city you want to know weather")
+
+# check API documentation to see what structure of URL is needed to access the data
+# http://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
+url = 'http://api.openweathermap.org/data/2.5/weather?q=' + location + '&appid=' + APIkey
+# print(url)
+
+
+# Download the JSON data from OpenWeatherMap.org's API.
+response = requests.get(url)  
+# Uncomment to see the raw JSON text:
+# print(response.text)  
+
+
+# Load JSON data into a Python variable.
+weatherData = json.loads(response.text)
+# Uncomment to see the raw JSON text:
+#print(weatherData) 
+# from pprint import pprint 
+# pprint(weatherData) 
+
+st.write(weatherData['main']['temp_max']) 
