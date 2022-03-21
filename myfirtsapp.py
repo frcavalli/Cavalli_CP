a27ab53644c9fd4e28e9e9d75f16b0f3d77d5609
frcavalli <101572991+frcavalli@users.noreply.github.com>
2022-03-21 15:48:32 +0100

    Update myfirtsapp.py

--- myfirtsapp.py
+++ myfirtsapp.py
@@ -6,7 +6,7 @@ st.write('The current movie title is', title)
 
 import streamlit as st
 genre = st.radio("What's your favorite movie genre",('Comedy', 'Drama', 'Documentary'))
- if genre == 'Comedy':
-     st.write('You selected comedy.')
- else:
-     st.write("You didn't select comedy.")
+if genre == 'Comedy':
+    st.write('You selected comedy.')
+else:
+    st.write("You didn't select comedy.")
