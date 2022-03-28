77a157b3e344ea697710158467c5c23ed4cece69
frcavalli <101572991+frcavalli@users.noreply.github.com>
2022-03-28 16:31:17 +0200

    Update CP_4.py

--- CP_4.py
+++ CP_4.py
@@ -1,10 +1,12 @@
 import streamlit as st 
 import json, requests
 st.header("Your choice") 
-title = st.text_input('write a word', 'lorem ipsum')
-st.write('your word is', title)
+title = st.text_input('Write here your word', 'lorem ipsum')
+st.write('Your word is', title)
 
-APIkey = "914e56ce07698c06f712a3cad0747080"
+#APIkey = "914e56ce07698c06f712a3cad0747080"
 
-url= 'https://api.datamuse.com/words?rel_rhy=' + title
+url= 'https://api.datamuse.com/words?rel_rhy=' + title + '&max=5'
 
+option = st.selectbox('Which option do you want to select?', ('Synonyms', 'Antonyms', 'Sounds like' 'Means like')) 
+st.write('You selected:', option)
