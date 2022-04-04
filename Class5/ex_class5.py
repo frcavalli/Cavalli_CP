0c5bd917aeed9513a2b1fd858718389bd5b546b4
frcavalli <101572991+frcavalli@users.noreply.github.com>
2022-04-04 16:16:06 +0200

    Update ex_class5.py

--- Class5/ex_class5.py
+++ Class5/ex_class5.py
@@ -1,1 +1,14 @@
+from googletrans import Translator 
 
+translator=Translator()
+
+st.header("Translator") 
+word = st.text_input('Write here your word or phrase', 'lorem ipsum')
+st.write('Your word is', word)
+  
+if word != 'nothing':
+  lang= input('Give me a target language ')
+  trans_it= translator.translate(word, dest= lang)
+  st.write('the translation of this word in', trans_it.dest, 'is', trans_it.text)
+else:
+  st.write('you did not write any word')
