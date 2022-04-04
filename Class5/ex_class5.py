781169d2b8af2d2aa39206d67c67d992935c482f
frcavalli <101572991+frcavalli@users.noreply.github.com>
2022-04-04 16:23:20 +0200

    Update ex_class5.py

--- Class5/ex_class5.py
+++ Class5/ex_class5.py
@@ -8,7 +8,7 @@ word = st.text_input('Write here your word or phrase', ' ')
 st.write('Your word is', word)
   
 if word != 'nothing':
-  lang= input('Give me a target language ')
+  lang= st.text_input('Give me a target language ')
   trans_it= translator.translate(word, dest= lang)
   st.write('the translation of this word in', trans_it.dest, 'is', trans_it.text)
 else:
