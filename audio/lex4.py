d7de662b0c77c9c9f3dedb41ef671e7674448cf7
frcavalli <101572991+frcavalli@users.noreply.github.com>
2022-04-26 12:24:28 +0200

    Update lex4.py

--- audio/lex4.py
+++ audio/lex4.py
@@ -1,1 +1,15 @@
+from gtts import gTTS
+import IPython.display as ipd  
+from googletrans import Translator 
+translator=Translator()
 
+text= input('Write here the text: ')
+lang= input('write here a 2-letter language code: ')
+trans_text= translator.translate(text, dest= lang)
+
+tts1=gTTS(trans_text.text, lang)
+tts1.save("audiofile.mp3")
+
+
+print('Your Audio:')
+ipd.display(ipd.Audio('audiofile.mp3'))
