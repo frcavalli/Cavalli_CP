40fbf082909ae44d90914ecac5942f8c03062ba9
frcavalli <101572991+frcavalli@users.noreply.github.com>
2022-04-26 12:41:20 +0200

    Update lex4.py

--- audio/lex4.py
+++ audio/lex4.py
@@ -2,15 +2,32 @@ import streamlit as st
 from gtts import gTTS
 import IPython.display as ipd  
 from googletrans import Translator 
+
 translator=Translator()
 
-text= input('Write here the text: ')
-lang= input('write here a 2-letter language code: ')
-trans_text= translator.translate(text, dest= lang)
+st.header("Translator")
 
-tts1=gTTS(trans_text.text, lang)
-tts1.save("audiofile.mp3")
+text= st.input('Write here the text you want to translate: ', ' ')
+
+if text != ' ':
+  st.write('Your text is: ', text)
+  if text != 'nothing':
+    lang= st.text_input('Give me a 2-letter target language ', 'en')
+    trans_text= translator.translate(text, dest= lang)
+    st.write('the translation of this text in', trans_text.dest, 'is: ', trans_text.text)
+    
+    tts1=gTTS(trans_text.text, lang)
+    tts1.save("audiofile.mp3")
+    my_audio= open("audiofile.mp3", "rb")
+    st.write('Your Audio:')
+    
+    st.audio(data=audio_file, format="audio/mp3", start_time=0)
+    
+    #st.audio(ipd.display(ipd.Audio('audiofile.mp3')))
+    
+  else:
+    st.write('you did not write any word')
 
 
-print('Your Audio:')
-ipd.display(ipd.Audio('audiofile.mp3'))
+
+
