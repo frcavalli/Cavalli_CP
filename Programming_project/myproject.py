16fd6573f27d7174a741388f98b966a599743f29
frcavalli <101572991+frcavalli@users.noreply.github.com>
2022-05-09 15:22:39 +0200

    Update myproject.py

--- Programming_project/myproject.py
+++ Programming_project/myproject.py
@@ -1,1 +1,9 @@
+import streamlit as st
+from gtts import gTTS
+import IPython.display as ipd  
+from googletrans import Translator 
+translator=Translator()
+import speech_recognition as sr
 
+
+st.header("My Project")
