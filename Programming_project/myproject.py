8251ab2976385b4ba5aacb73ff5015c279e54fae
frcavalli <101572991+frcavalli@users.noreply.github.com>
2022-05-09 15:59:00 +0200

    Update myproject.py

--- Programming_project/myproject.py
+++ Programming_project/myproject.py
@@ -5,5 +5,17 @@ from googletrans import Translator
 translator=Translator()
 import speech_recognition as sr
 
-
+#title
 st.header("My Project")
+
+#uploadfile and Speech recognition
+r = sr.Recognizer()
+AUDIO_FILE = "sample_audio_short.wav"   #mp3 files are not supported
+
+with sr.AudioFile(AUDIO_FILE) as source:
+    audio = r.record(source)  
+
+recognised_text= r.recognize_google(audio)
+
+print('the text recognized from the audio seems to be: ')
+print( recognised_text)
