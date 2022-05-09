0e1ee12e86cf1dd8ef3e92d7363c33ffc875164c
frcavalli <101572991+frcavalli@users.noreply.github.com>
2022-05-09 16:25:26 +0200

    Update myproject.py

--- Programming_project/myproject.py
+++ Programming_project/myproject.py
@@ -10,11 +10,28 @@ st.header("My Project")
 
 #uploadfile and Speech recognition
 r = sr.Recognizer()
-AUDIO_FILE = "OSR_us_000_0011_8k.wav"   
-with sr.AudioFile(AUDIO_FILE) as source:
+audio_file = st.file_uploader("Upload your audio file")
+if audio_file is not None: 
+    coll_data= audio_file.getvalue()
+    st.write(coll_data)
+    
+    stringio=StringIO(audio_file.getvalue().decode("utf-8"))
+     st.write(stringio)
+     
+    new_string = stringio.read()
+     st.write(new_string)
+
+     # Can be used wherever a "file-like" object is accepted:
+     dataframe = pd.read_csv(audio_file)
+     st.write(dataframe)
+    
+    
+with sr.AudioFile(audio_file) as source:
     audio = r.record(source)  
 
 recognised_text= r.recognize_google(audio)
 
 st.write('the text recognized from the audio seems to be: ')
 st.write( recognised_text)
+
+
