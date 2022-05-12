36e36f1680e8e787bdabe3d8cf348db8d776a20c
frcavalli <101572991+frcavalli@users.noreply.github.com>
2022-05-12 14:07:19 +0200

    Update myproject.py

--- Programming_project/myproject.py
+++ Programming_project/myproject.py
@@ -38,26 +38,26 @@ else:
 
 
 lang = st.selectbox('Choose a 2-letter target language: ', ('italien', 'spanish', 'german', 'croatian', 'french'))
-   if lang == 'italien':
-      code= 'it'
-   elif lang == 'spanish':
-      code= 'es'
-   elif lang == 'german':
-      code= 'de'
-   elif lang == 'croatian':
-      code= 'hr'
-   elif lang== 'french':
-      code= 'fr'
+if lang == 'italien':
+   code= 'it'
+elif lang == 'spanish':
+   code= 'es'
+elif lang == 'german':
+   code= 'de'
+elif lang == 'croatian':
+   code= 'hr'
+elif lang== 'french':
+   code= 'fr'
    
-      trans_text= translator.translate(recognised_text, dest= code)
-      st.write('the translation of this text in', trans_text.dest, 'is: ', trans_text.text)
+   trans_text= translator.translate(recognised_text, dest= code)
+   st.write('the translation of this text in', trans_text.dest, 'is: ', trans_text.text)
     
-      tts1=gTTS(trans_text.text, code)
-      tts1.save("audiofile.mp3")
-      my_audio= open("audiofile.mp3", "rb")
-      st.write('Your Audio:')
+   tts1=gTTS(trans_text.text, code)
+   tts1.save("audiofile.mp3")
+   my_audio= open("audiofile.mp3", "rb")
+   st.write('Your Audio:')
     
-      st.audio(data=my_audio, format="audio/mp3", start_time=0)
-    
-   else:
-      st.write('you did not write any word')
+   st.audio(data=my_audio, format="audio/mp3", start_time=0)
+ 
+else:
+   st.write('you did not write any word')
