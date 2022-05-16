2b2a63daca4f00bd5dd64ff8da04b3d28e5e0808
frcavalli <101572991+frcavalli@users.noreply.github.com>
2022-05-16 16:53:41 +0200

    Update myproject.py

--- Programming_project/myproject.py
+++ Programming_project/myproject.py
@@ -52,8 +52,8 @@ if audio_file is not None:
          trans_text= translator.translate(recognised_text, dest= code)
          st.write('the translation of this text in', trans_text.dest, 'is: ', trans_text.text)
         
-    tts1=gTTS(trans_text.text, code)
-    tts1.save("audiofile.mp3")
+    ttmp3=gTTS(trans_text.text, lang=code, tld='com')
+    ttmp3.save("audiofile.mp3")
     my_audio= open("audiofile.mp3", "rb")
     st.write('Here is the audio of your translated text:')
 
