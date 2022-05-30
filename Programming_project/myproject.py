82389bed79600278edb7c57cd3b0319d076de89f
frcavalli <101572991+frcavalli@users.noreply.github.com>
2022-05-30 17:13:41 +0200

    Update myproject.py

--- Programming_project/myproject.py
+++ Programming_project/myproject.py
@@ -9,14 +9,8 @@ import speech_recognition as sr
 #title
 st.header("Audio file translator")
 
-from PIL import Image 
-image= Image.open("https://raw.githubusercontent.com/frcavalli/Cavalli_CP/main/Programming_project/immagine_progetto.jpg")
-image= image.resize((500,250).Image.ANTIALIAS)
-image.save(fp="banner.jpg")
-
-#urlfoto= "https://raw.githubusercontent.com/frcavalli/Cavalli_CP/main/Programming_project/immagine_progetto.jpg"
-
-#st.image(urlfoto)
+urlfoto= "https://raw.githubusercontent.com/frcavalli/Cavalli_CP/main/Programming_project/immagine_progetto.jpg"
+st.image(urlfoto)
 
 st.caption('This application serves to translate an audio file into another language. Enter an audio file from your device in the box below and the application will translate it for you. The application is designed for English students trying to translate audio files into another language for study purposes, but can also be useful for tourists and people with other purposes.')
     
